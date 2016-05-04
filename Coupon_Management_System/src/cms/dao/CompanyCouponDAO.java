153382a63f1abf60ecb4732539d58a870f5bbd9e
levyoav <levyoav28@gmail.com>
2016-05-04 17:49:41 +0300

    Auto indented class CompanyCouponDAO.

--- Coupon_Management_System/src/cms/dao/CompanyCouponDAO.java
+++ Coupon_Management_System/src/cms/dao/CompanyCouponDAO.java
@@ -5,19 +5,19 @@ import java.util.Collection;
 import cms.main.CouponSystemException;
 
 public interface CompanyCouponDAO {
-	
+
 	public void insertRow(long cmpnyId, long cpnId) throws CouponSystemException;
-	
+
 	public void deleteRow(String clmnName, long id) throws CouponSystemException;
-	
+
 	public void deleteCoupon(long cpnId) throws CouponSystemException;
 
 	public void deleteCouponsByCompanyId(long cmpnyId) throws CouponSystemException;
-	
+
 	public long getCompanyIDByCouponID(long cpnId) throws CouponSystemException;
-	
+
 	public Collection<Long> getCouponsIDsByCompanyID(long cmpnyId) throws CouponSystemException;
-	
+
 	public Collection<long[]> getAllRows() throws CouponSystemException;
-	
+
 }
